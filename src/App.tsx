3daa55dea32aa90d4c258c57b70d031197c89368
AdamWarfa <Adamwarfadk@gmail.com>
2024-04-30 21:26:42 +0200

    lille test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import TestPost from "./components/TestPost";
+import TestContent from "./components/TestContent";
+
 function App() {
   return (
     <>
       <p>Velkommen til App.tsx</p>
+      <TestPost />
+      <TestContent />
     </>
   );
 }
