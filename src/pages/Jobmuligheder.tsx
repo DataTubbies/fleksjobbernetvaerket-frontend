238b5e9298d689aa625d1208cee7059fa5487d58
Yousra Diab <yousserah@gmail.com>
2024-05-15 14:54:55 +0200

    FLEK-25 done

--- src/pages/Jobmuligheder.tsx
+++ src/pages/Jobmuligheder.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import { fetchJobs } from "@/api/wp-rest";
+import ColorBox from "@/components/ColorBox";
 
 interface Job {
 acf: {
   jobtitel: string;
-  jobeskrivelse: string;
+  Jobbeskrivelse: string;
   jobtype: string;
   jobreference: string;
   url: string;
@@ -44,20 +45,35 @@ export default function Jobmuligheder() {
     
   }, []);
 
+  const colorBoxObj = {
+    title: "JOBMULIGHEDER",
+    description: "Her kan du finde seneste jobsopslag fra Fleksjobber Netværket. Vi opdaterer løbende med nye jobmuligheder, så hold øje med denne side, hvis du er på udkig efter et nyt job.",
+    reversed: true,
+  };
 
   return (
 <>
-   
+      <ColorBox {...colorBoxObj} />
+  
+  <div className="grid grid-cols-1 grid-rows-2 justify-center py-8 mx-12 md:mx-32 my-10">
+
       {jobs.map((job) => (
-        <div key={job.acf.jobtitel}>
-          <p>{job.acf.jobtitel}</p>
-          <p>{job.acf.jobeskrivelse}</p>
-          <p>{job.acf.jobtype}</p>
-          <p>{job.acf.jobreference}</p>
-          <a href={job.acf.url}>{job.acf.url}</a>
+        <div className="border-solid border-b-4 border-fleks-grey" key={job.acf.jobtitel}>
+          <p className="font-bold text-2xl text-fleks-blue-dark my-6">{job.acf.jobtitel}</p>
+          <p className="my-7"><span className="font-semibold">Jobtype: </span>{job.acf.jobtype}</p>
+          <p className="my-7"><span className="font-semibold">Jobreference: </span>{job.acf.jobreference}</p>
+
+
+          <p className="my-7"> <span className="font-semibold">Jobbeskrivelse: </span>{job.acf.Jobbeskrivelse}</p>
+          {job.acf.url && (
+              <a className="font-semibold" href={job.acf.url}>Læs mere her</a>
+            )}
         </div>
+      
+
       ))}
-   
+      </div>
+
     </>
   );
 }
