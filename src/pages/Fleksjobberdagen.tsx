f520f3e8800b5fa353df8d312f3d3ab2f6ceaf71
Mads <mk@magnutron.dk>
2024-05-20 18:46:36 +0200

    Update Fleksjobberdagen.tsx

--- src/pages/Fleksjobberdagen.tsx
+++ src/pages/Fleksjobberdagen.tsx
@@ -12,6 +12,7 @@ export default function Fleksjobberdagen() {
       createArrangorerImageArray(data[0].acf);
       createPolitikerArray(data[0].acf);
       createPartierImageArray(data[0].acf);
+      createtilmeldPunkterArray(data[0].acf);
       setContent(data[0].acf);
     });
   }, []);
@@ -20,30 +21,54 @@ export default function Fleksjobberdagen() {
     <>
       <ColorBox title={content.bannerTitle} description={content.bannerText} reversed={false} />
       <div id="arrangorer" className="justify-items-center">
-        <h1 className="text-xl font-bold text-center py-8">{content.arrangorerTitle}</h1>
+        <h1 className="text-xl font-bold text-center py-8 px-12 lg:px-32">{content.arrangorerTitle}</h1>
         <div id="arrangorer-grid" className="grid grid-cols-1 px-12 md:grid-cols-2 lg:px-32 lg:grid-cols-4 gap-4 justify-items-center items-center">
           {content.arrangorerImages && content.arrangorerImages.map((img: string, index: number) => <img key={index} src={img} alt="arrangorer" className="max-w-full max-h-40 object-contain mx-auto" />)}
         </div>
       </div>
       <div id="poltikere" className="justify-items-center">
-        <h1 className="text-xl font-bold text-center py-8">{content.politikereTitle}</h1>
-        <div id="politikere-grid" className="grid grid-cols-1 px-12 md:grid-cols-2 lg:px-32 lg:grid-cols-4 gap-1 justify-items-center items-center">
+        <h1 className="text-xl font-bold text-center py-8 px-12 lg:px-32">{content.politikereTitle}</h1>
+        <div id="politikere-grid" className="grid grid-cols-1 px-12 md:grid-cols-3 lg:px-32 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 gap-1 justify-items-center items-center">
           {content.politikere &&
             content.politikere.map((politiker: any, index: number) => (
-              <div key={index} className="flex flex-col items-center bg-fleks-gray w-60 pb-3 h-60 items">
-                <img src={politiker.img} alt="politiker" className="max-w-full max-h-40 object-contain mx-auto pb-2" />
-                <p className="text-center">{politiker.text}</p>
+              <div key={index} className="flex flex-col bg-fleks-gray w-48 pb-3 h-60 rounded">
+                <img src={politiker.img} alt="politiker" className="max-w-full max-h-40 object-contain mx-auto py-2" />
+                <p className="text-center inset-x-0 bottom-0">{politiker.text}</p>
               </div>
             ))}
         </div>
-        <div id="partier" className="grid grid-cols-1 px-12 md:grid-cols-2 lg:px-32 lg:grid-cols-4 gap-4 justify-items-center items-center">
+        <div id="partier" className="grid grid-cols-1 px-12 md:grid-cols-2 lg:px-32 lg:grid-cols-4 gap-4 justify-items-center items-center py-3">
           {content.partiLogoer && content.partiLogoer.map((img: string, index: number) => <img key={index} src={img} alt="parti" className="max-w-full max-h-40 object-contain mx-auto" />)}
+        </div>
+        <div id="tilmeld" className="grid grid-cols-1 justify-items-center border-t-[3px] px-12 py-5 lg:px-32">
+          <a href={content.tilmeldArrangorerLink} className="bg-fleks-blue text-white font-bold py-2 px-4 rounded-full">
+            Tilmeld dig som arrangør her
+          </a>
+          <h1 className="text-xl font-bold text-center py-8">{content.tilmeldTitle}</h1>
+          <ol>
+            {content.tilmeldPunkter &&
+              content.tilmeldPunkter.map((punkt: string, index: number) => (
+                <li key={index} className="py-2">
+                  &#8226; {punkt}
+                </li>
+              ))}
+          </ol>
+          <p className="text-center py-3">
+            <a href={content.laesMereLink}>
+              <u>Læs mere om fleksjobberdagen</u>
+            </a>{" "}
+            og gå til{" "}
+            <a href={content.tilmeldArrangorerLink}>
+              <u>tilmelding for arrangører her.</u>
+            </a>
+          </p>
         </div>
       </div>
     </>
   );
 }
 
+// There is a duplicate function later. Might aswell combine that to a single..
 function createArrangorerImageArray(data: any) {
   let imageArray = [];
   for (let i = 1; i < 10; i++) {
@@ -92,5 +117,19 @@ function createPartierImageArray(data: any) {
   imageArray.filter((str) => str !== "");
 
   data.partiLogoer = imageArray;
+  return data;
+}
+
+function createtilmeldPunkterArray(data: any) {
+  let punkterArray = [];
+  for (let i = 1; i < 10; i++) {
+    if (data[`tilmeldPunkt${i}`]) {
+      punkterArray.push(data[`tilmeldPunkt${i}`]);
+    }
+    delete data[`tilmeldPunkt${i}`];
+  }
+  punkterArray.filter((str) => str !== "");
+
+  data.tilmeldPunkter = punkterArray;
   return data;
 }
