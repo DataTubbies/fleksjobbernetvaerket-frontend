e63427e67f38f124e40184924ae4dc8155b87f24
AdamWarfa <Adamwarfadk@gmail.com>
2024-05-21 12:43:44 +0200

    FLEK-27

--- src/pages/Persondatapolitik.tsx
+++ src/pages/Persondatapolitik.tsx
@@ -1,8 +1,32 @@
 export default function Persondatapolitik() {
   return (
-    <div>
-      <h1>Persondatapolitik</h1>
-      <p>Her kan du læse om vores persondatapolitik</p>
+    <div className="px-32 py-12">
+      <div>
+        <h2 className="text-3xl">Persondatapolitik</h2>
+        <p>Hos Fleksjobber Netværket bestræber vi os altid på at beskytte dine og andres personoplysninger bedst muligt. Det gør vi, fordi vi gerne vil værne om den grundlæggende rettighed til privatlivets fred.</p>
+        <br />
+        <p>
+          Her i vores persondatapolitik, kan du finde information om hvordan vi behandler og beskytter personoplysninger, når du er besøgende hos os eller benytter dig af vores services, herunder hjemmesiden,
+          nyhedsbreve, Sociale Medier mv.
+        </p>
+        <h2 className="text-3xl">Kontakt og persondataansvarlig</h2>
+        <p>
+          Fleksjobber Netværke er dataansvarlig for de personoplysninger der behandles, når du er kunde hos os eller benytter dig af vores services, herunder hjemmesiden, nyhedsbreve, Sociale Medier mv. Du kan til en
+          hver tid rette henvendelse via nedenstående. Hvis din henvendelse vedrører indsigt, sletning, berigtigelse, begrænsning, indsigelse eller dataportabilitet, så læs venligst afsnittet om “Dine rettigheder” først.
+        </p>
+        <br />
+        <h2 className="text-3xl">Hvilke personoplysninger behandler vi og til hvilket formål</h2>
+        <p>
+          Afhængig af din relation til os, behandler vi dine personoplysninger på forskellig måde og til forskellige formål. Du kan herunder folde overskrifterne ud, for at læse hvad der gælder for det specifikke område.
+        </p>
+        <br />
+        <p>
+          SE/CVR nummer: 37109223 - Se data på cvr.dk via dette{" "}
+          <a className="text-fleks-blue-dark" href="https://cvrapi.dk/virksomhed/dk/fleksjobber-netvaerket/37109223">
+            link.
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
