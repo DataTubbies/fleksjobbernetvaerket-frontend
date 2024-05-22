06e9bf5660d224775f863f012d4d035770847d64
Mads <mk@magnutron.dk>
2024-05-22 11:33:01 +0200

    yes

--- src/pages/Artikler.tsx
+++ src/pages/Artikler.tsx
@@ -4,6 +4,30 @@ import PostEntry from "@/components/PostEntry";
 
 export default function Artikler() {
   const [posts, setPosts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSort = () => {
+    const newDirection = sortDirection === "asc" ? "desc" : "asc";
+    setSortDirection(newDirection);
+
+    const sortedOrdbog = [...posts].sort((a, b) => {
+      const ordA = a.title.toLowerCase();
+      const ordB = b.title.toLowerCase();
+
+      if (newDirection === "asc") {
+        return ordA < ordB ? -1 : ordA > ordB ? 1 : 0;
+      } else {
+        return ordA > ordB ? -1 : ordA < ordB ? 1 : 0;
+      }
+    });
+
+    setPosts(sortedOrdbog);
+  };
 
   useEffect(() => {
     async function getPosts() {
@@ -16,8 +40,10 @@ export default function Artikler() {
 
   return (
     <div>
+      <img src="../../public/images/sortArrows.svg" alt="Sort Arrows" onClick={handleSort} className="ml-2 cursor-pointer" />
+      <input type="text" placeholder="Søg efter begreb..." value={searchQuery} onChange={handleSearch} className="border rounded px-2 hover:border-fleks-blue focus:border-fleks-blue focus:outline-none w-full h-8" />
       {posts.map((post) => (
-        <PostEntry key={post.id} title={post.title.rendered} excerpt={post.excerpt.rendered} date={post.date} author={post.author} />
+        <PostEntry key={post.id} title={post.title.rendered} excerpt={post.excerpt.rendered} date={post.date} author={post.author} link={post.link} slug={post.slug} />
       ))}
     </div>
   );
