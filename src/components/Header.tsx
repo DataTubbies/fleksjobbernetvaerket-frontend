ae186228ec30916c4fdc50a3aeedefae0eb42dd0
AdamWarfa <Adamwarfadk@gmail.com>
2024-05-22 10:30:56 +0200

    FLEK-32 Hjælp tilføjet

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,5 @@
 export default function Header() {
   return (
-    <header className="flex items-center justify-between bg-[#242424] p-4 sm:p-6">
-      <div className="w-36 sm:w-72">
-        <img
-          className="invert brightness-[86%] grayscale w-full"
-          src="https://www.fleksjobbernetvaerket.dk/wp-content/uploads/2014/09/Fleksjobber-Netv%C3%A6rket-logo_rgb.jpg"
-          alt="Logo"
-        />
-      </div>
-      <div>
-        <button className="text-lg sm:text-xl font-bold px-4 sm:px-8 py-2 sm:py-4 bg-fleks-blue text-white rounded-[25px]">
-          STØT OS <span className="text-xl sm:text-2xl"> ♡</span>
-        </button>
-      </div>
-
     <header className="grid grid-cols-2 bg-[#242424]">
       <section>
         <div className=" w-48 md:w-72">
@@ -27,7 +13,6 @@ export default function Header() {
           </button>
         </a>
       </section>
-
     </header>
   );
 }
